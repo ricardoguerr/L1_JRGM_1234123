caf5e60367b072f2f7b4650052354e043bc3ddf2
Ricardo <Ricardo@LAPTOP-H62KMCD7>
2023-02-18 11:23:01 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hola mundo soy Ricardo");
+   
+
+        Console.WriteLine("Hola mundo");
+        Console.WriteLine("Soy Ricardo");
+
+        /* COMENTARIO */
+
+        Console.Write("Hola mundo ");
+        Console.Write("soy Ricardo\n");
+
+        Console.WriteLine("\nIngrese su nombre: ");
+        string nombre = Console.ReadLine();
+        Console.Write("Hola mundo ");
+        Console.Write("soy " + nombre);
+        Console.ReadKey();
+    }
+}
